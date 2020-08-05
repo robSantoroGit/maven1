91b23476ec61557e201c9057446d1589ce63824f
Roberto Santoro <robesantoro@gmail.com>
2020-08-05 19:30:33 +0200

    Commit progetto Maven

---
+++ src/main/java/com/azienda/progettoMavenEsercizioBase/App.java
@@ -0,0 +1,13 @@
+package com.azienda.progettoMavenEsercizioBase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
